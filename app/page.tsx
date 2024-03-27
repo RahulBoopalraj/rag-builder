65a4476b307a0c8f7f7bdad87b3ab7c9e29f19a6
RahulDev2004 <talktorahul.b@gmail.com>
2024-03-27 12:50:51 +0530

    added login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { Dashboard } from "./pages/Dashboard/page";
 
 export default function Home() {
   return (
     <div>
-      <Dashboard />
-    </div>  );
+      <h1>Test</h1>
+    </div>  
+  );
 }
