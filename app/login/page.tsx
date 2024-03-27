e43cffc441275f6e0b0c09009576c5dfd71f88c0
sabari245 <sabari2004@hotmail.com>
2024-03-27 14:16:01 +0530

    minor updates in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
 import { toast } from "sonner"
@@ -5,15 +7,16 @@ import { toast } from "sonner"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Separator } from "@/components/ui/separator"
 
 export default function Login() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
+          <div className="grid gap-2 text-center mb-3">
+            <h1 className="text-3xl font-bold">Welcome</h1>
+            <p className="text-sm text-muted-foreground">
               Enter your email below to login to your account
             </p>
           </div>
@@ -27,49 +30,46 @@ export default function Login() {
                 required
               />
             </div>
-            {/* <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
-            </div> */}
             <Button
-                type="submit" className="w-full"
-                variant="outline"
-                onClick={() =>
+              type="submit" className="w-full"
+              onClick={() => (
                 toast("Event has been created", {
-                description: "Sunday, December 03, 2023 at 9:00 AM",
-                action: {
+                  description: "Sunday, December 03, 2023 at 9:00 AM",
+                  action: {
                     label: "Undo",
-                onClick: () => console.log("Undo"),
-                    },
-                })
-                }>Login</Button>
-             <Button
-                type="submit" className="w-full"
-                variant="outline"
-                onClick={() =>
+                    onClick: () => console.log("Undo"),
+                  },
+                }))
+              }>Login</Button>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or continue with
+                </span>
+              </div>
+            </div>
+            <Button
+              type="submit" className="w-full"
+              variant="outline"
+              onClick={() => (
                 toast("Event has been created", {
-                description: "Sunday, December 03, 2023 at 9:00 AM",
-                action: {
+                  description: "Sunday, December 03, 2023 at 9:00 AM",
+                  action: {
                     label: "Undo",
-                onClick: () => console.log("Undo"),
-               },
-            })
-            }> Login with Google </Button>
+                    onClick: () => console.log("Undo"),
+                  },
+                }))
+              }> Login with Google </Button>
           </div>
-          {/* <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
+          <div className="mt-4 text-center text-sm">
+            By signing in you agree with our{" "}
             <Link href="#" className="underline">
-              Sign up
+              terms and conditions
             </Link>
-          </div> */}
+          </div>
         </div>
       </div>
       <div className="hidden bg-muted lg:block">
