63a019b54279c24a244477e18b3cd7be0222bff6
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-03-28 17:44:46 +0530

    refactor(landing): update layout, add img

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-const inter = Inter({ subsets: ["latin"] });
+const siteFont = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={siteFont.className}>
         <TooltipProvider>
           {children}
         </TooltipProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -3,126 +3,141 @@ import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Check } from "lucide-react";
-import { CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-
-
+import {
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+} from "@/components/ui/card";
+import { Boxes } from "@/components/aceternity/background-boxes";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      <header className="px-4 lg:px-6 h-14 flex items-center">
+      <header className="px-4 lg:px-6 pt-9 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
           <span>Acme Inc</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="#" >
             Features
           </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/pricing" >
             Pricing
           </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="#" >
             About
           </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="#" >
             Contact
+          </Link>
+          <Link href="/login">
+            <button>Login</button>
           </Link>
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full min-h-[calc(100dvh-3.5rem)] flex items-center justify-center">
-          <div className="container px-4 md:px-6">
-            <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
-              <Image
-                alt="Image"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-                height="310"
-                src="/placeholder.svg"
-                width="550"
-              />
-              <div className="flex flex-col justify-center space-y-4">
-                <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                  Build AI-powered chatbots without <span className="text-blue-500">a single line of code!</span>
-                </h1>
-                <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  Create intelligent chatbots without writing a single line of
-                  code. Engage your customers with automated conversational
-                  experiences. Let your imagination flow while we handle the
-                  technology.
-                </p>
-                <Link
-                  className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300 sm:max-w-52"
-                  href="#"
-                >
-                  Get Started <ArrowRight className="ml-2 h-4 w-4" />
-                </Link>
-              </div>
+        <section className="w-full min-h-[calc(100dvh-3.5rem)] flex items-center justify-center relative overflow-hidden">
+          {/* <Boxes /> */}
+          <svg
+            aria-hidden="true"
+            className="absolute inset-x-0 -top-20 -z-10 h-[1000px] w-full fill-neutral-50 stroke-neutral-900/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+          >
+            <rect width="100%" height="100%" fill="url(#:R4d6:)" strokeWidth={0} />
+            <svg x="50%" y={-96} strokeWidth={0} className="overflow-visible">
+              <path transform="translate(64 160)" d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z" />
+              <path transform="translate(128 320)" d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z" />
+              <path transform="translate(288 480)" d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z" />
+              <path transform="translate(512 320)" d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z" />
+              <path transform="translate(544 640)" d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z" />
+              <path transform="translate(320 800)" d="M45.119 4.5a11.5 11.5 0 0 0-11.277 9.245l-25.6 128C6.82 148.861 12.262 155.5 19.52 155.5h63.366a11.5 11.5 0 0 0 11.277-9.245l25.6-128c1.423-7.116-4.02-13.755-11.277-13.755H45.119Z" />
+            </svg>
+            <defs>
+              <pattern id=":R4d6:" width={96} height={480} x="50%" patternUnits="userSpaceOnUse" patternTransform="translate(0 -96)" fill="none" >
+                <path d="M128 0 98.572 147.138A16 16 0 0 1 82.883 160H13.117a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-45.117 320H-116M64-160 34.572-12.862A16 16 0 0 1 18.883 0h-69.766a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-109.117 160H-180M192 160l-29.428 147.138A15.999 15.999 0 0 1 146.883 320H77.117a16 16 0 0 0-15.69 12.862L34.573 467.138A16 16 0 0 1 18.883 480H-52M-136 480h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1-18.883 320h69.766a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 109.117 160H192M-72 640h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 45.117 480h69.766a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A15.999 15.999 0 0 1 173.117 320H256M-200 320h58.883a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A16 16 0 0 1-82.883 160h69.766a16 16 0 0 0 15.69-12.862L29.427 12.862A16 16 0 0 1 45.117 0H128" />
+              </pattern>
+            </defs>
+          </svg>
+
+          <div className="container px-4 md:px-6 z-20 overflow-x-hidden flex items-stretch flex-col-reverse md:flex-row">
+            <div className="flex flex-col justify-center space-y-4">
+              <h1 className="lg:leading-snug text-3xl font-bold tracking-tight sm:text-3xl md:text-4xl xl:text-5xl 2xl:text-7xl">
+                Turn Visitors into Customers with Conversational AI
+              </h1>
+              <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                Engage your customers with automated conversational
+                experiences. Turn Conversations into Conversions with RAG Builder.
+              </p>
+              <Link
+                className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300 sm:max-w-52"
+                href="#"
+              >
+                Get Started <ArrowRight className="ml-2 h-4 w-4" />
+              </Link>
             </div>
+            <Image
+              alt="Image"
+              className="mx-auto aspect-square overflow-hidden rounded-xl object-cover object-center sm:w-full h-auto lg:order-last"
+              height="1041"
+              src="/landing.png"
+              width="1041"
+            />
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full py-12">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-                  New Features
-                </div>
+                {/* <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
+                  Revolutionize
+                </div> */}
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Faster iteration. More innovation.
+                  Maximize Customer Retention.
                 </h2>
                 <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  The platform for rapid progress. Let your team focus on
-                  shipping features instead of managing infrastructure with
-                  automated CI/CD, built-in testing, and integrated
-                  collaboration.
+                  Empower your business with conversational AI to build lasting
+                  relationships, increase engagement, and drive loyalty. Let the
+                  technology handle customer interactions while you focus on
+                  growth strategies and maximizing revenue.
                 </p>
               </div>
             </div>
             <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
               <Image
                 alt="Image"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-                height="310"
-                src="/placeholder.svg"
+                className="mx-auto aspect-square overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
+                height="550"
+                src="/landing.png"
                 width="550"
               />
               <div className="flex flex-col justify-center space-y-4">
                 <ul className="grid gap-6">
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Collaboration</h3>
+                      <h3 className="text-xl font-bold">
+                        Personalized Interaction
+                      </h3>
                       <p className="text-gray-500 dark:text-gray-400">
-                        Make collaboration seamless with built-in code review
-                        tools.
+                        Tailor conversations to customer preferences and needs
+                        with ease.
                       </p>
                     </div>
                   </li>
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Automation</h3>
+                      <h3 className="text-xl font-bold">24/7 Availability</h3>
                       <p className="text-gray-500 dark:text-gray-400">
-                        Automate your workflow with continuous integration.
+                        Chatbots stay online 247, providing uninterrupted
+                        support and assistance.
                       </p>
                     </div>
                   </li>
                   <li>
                     <div className="grid gap-1">
-                      <h3 className="text-xl font-bold">Scale</h3>
+                      <h3 className="text-xl font-bold">Insights</h3>
                       <p className="text-gray-500 dark:text-gray-400">
-                        Deploy to the cloud with a single click and scale with
-                        ease.
+                        Gain valuable insights into customer behavior and
+                        preferences through analytics.
                       </p>
                     </div>
                   </li>
@@ -131,15 +146,15 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full py-12">
           <div className="container px-4 md:px-6">
             <div className="grid gap-10 sm:px-10 md:gap-16 md:grid-cols-2">
               <div className="space-y-4">
                 <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-                  Performance
+                  Build AI Chatbots in Minutes
                 </div>
                 <h2 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                  Traffic spikes should be exciting, not scary.
+                  Turn Conversations Into Conversions
                 </h2>
                 <Link
                   className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
@@ -150,25 +165,16 @@ export default function Home() {
               </div>
               <div className="flex flex-col items-start space-y-4">
                 <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-                  Security
+                  The Future of Customer Engagement
                 </div>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed dark:text-gray-400">
-                  Fully managed infrastructure designed to scale dynamically
-                  with your traffic, a global edge to ensure your site is fast
-                  for every customer, and the tools to monitor every aspect of
-                  your app.
+                  RAG Builder let&apos;s you build and deploy chatbots in minutes that help you engage with your customers and drive sales. With our platform turning conversations into conversions has never been easier.
                 </p>
-                <Link
-                  className="inline-flex h-9 items-center justify-center rounded-md border  border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                  href="#"
-                >
-                  Contact Sales
-                </Link>
               </div>
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-neutral-100 dark:bg-neutral-800">
+        {/* <section className="w-full py-12 md:py-24 lg:py-32 bg-neutral-100 dark:bg-neutral-800">
           <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
             <div className="space-y-3">
               <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
@@ -196,7 +202,9 @@ export default function Home() {
               </p>
             </div>
           </div>
-        </section>
+        </section> */}
+
+
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
